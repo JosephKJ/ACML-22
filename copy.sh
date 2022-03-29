0e72cb21f85eca1b315e88cba4aa3be4a97f8ec8
Joseph <josephkj20@gmail.com>
2022-03-29 22:31:00 +0530

    Updates

--- copy.sh
+++ copy.sh
@@ -1,1 +1,4 @@
-scp -i ~/id_rsa_acml2022 -r index.html acml2022@www.acml-conf.org:/home/acml2022/public_html
+scp -i ~/id_rsa_acml2022 -r *.html acml2022@www.acml-conf.org:/home/acml2022/public_html
+
+#scp -i ~/id_rsa_acml2022 -r assets/img/hero-bg.jpg acml2022@www.acml-conf.org:/home/acml2022/public_html/assets/img/
+
