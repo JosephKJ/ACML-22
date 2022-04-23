c9dc6804e80bf69629052b085ca73fc093f3cb56
bitbucket/manisha_dubey <cs17resch11003@iith.ac.in>
2022-04-23 13:43:11 +0530

    Submission Instructions Added

--- copy.sh
+++ copy.sh
@@ -1,4 +1,8 @@
-scp -i ~/id_rsa_acml2022 -r *.html acml2022@www.acml-conf.org:/home/acml2022/public_html
+scp -i id_rsa_acml2022 -r sponsorship.html acml2022@www.acml-conf.org:/home/acml2022/public_html
+
+
+
+# scp -i id_rsa_acml2022 -r assets/ACML2022-camera_ready-template.zip acml2022@www.acml-conf.org:/home/acml2022/public_html/assets/
 
 #scp -i ~/id_rsa_acml2022 -r assets/img/hero-bg.jpg acml2022@www.acml-conf.org:/home/acml2022/public_html/assets/img/
 
