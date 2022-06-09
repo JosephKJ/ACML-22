2593c96231823e33d76f057d6f19af01bd21fceb
Joseph <josephkj20@gmail.com>
2022-06-09 23:46:15 +0530

    MSR, KLA, JICA

--- copy.sh
+++ copy.sh
@@ -1,6 +1,6 @@
 scp -i ~/id_rsa_acml2022 -r *.html acml2022@www.acml-conf.org:/home/acml2022/public_html
 
-
+#scp -i ~/id_rsa_acml2022 -r assets/img/* acml2022@www.acml-conf.org:/home/acml2022/public_html/assets/img/
 
 # scp -i id_rsa_acml2022 -r assets/ACML2022-camera_ready-template.zip acml2022@www.acml-conf.org:/home/acml2022/public_html/assets/
 
