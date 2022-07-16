d8a8e7ef26b0787fd7bc41b768fce51c3b87b680
Joseph <josephkj20@gmail.com>
2022-07-16 11:53:31 +0530

    1) Updated the dates (Dec 12-14) on home page and CFP. 2) Updated OAMLS page: added speakers, added an organizer, added the dates. 3) Crossed off expired dates from CFP.

--- copy.sh
+++ copy.sh
@@ -1,4 +1,4 @@
-scp -i ~/id_rsa_acml2022 -r cfp.html acml2022@www.acml-conf.org:/home/acml2022/public_html
+scp -i ~/id_rsa_acml2022 -r *.html acml2022@www.acml-conf.org:/home/acml2022/public_html
 
 #scp -i ~/id_rsa_acml2022 -r assets/img/* acml2022@www.acml-conf.org:/home/acml2022/public_html/assets/img/
 
