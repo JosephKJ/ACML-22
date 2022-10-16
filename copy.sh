af2e2af0eb325a03c1352bd62a2013e0623b54d0
Joseph <josephkj20@gmail.com>
2022-10-16 11:36:38 +0530

    Newer homepage -- updates

--- copy.sh
+++ copy.sh
@@ -6,3 +6,4 @@ scp -i ~/id_rsa_acml2022 -r *.html acml2022@www.acml-conf.org:/home/acml2022/pub
 
 #scp -i ~/id_rsa_acml2022 -r assets/img/hero-bg.jpg acml2022@www.acml-conf.org:/home/acml2022/public_html/assets/img/
 
+#scp -i ~/id_rsa_acml2022 -r assets/css/style.css acml2022@www.acml-conf.org:/home/acml2022/public_html/assets/css/style.css
